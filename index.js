bde59e33094c34c28424d3512ff06ad3c7c235f8
Erik Luebbers <erluebbers@gmail.com>
2021-10-20 05:52:07 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+
+const input = document.getElementById('input')
+
+function clickMessage () {
+    alert('this page will self-destruct in ten seconds')
+}
+
 function addingEventListener() {
+    input.addEventListener('click', clickMessage)
 }
